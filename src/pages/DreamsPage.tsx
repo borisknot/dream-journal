7d1e5cd06d70fa7e8a4a27f9f90fafdec50885f8
mac4ever <borisknot@gmail.com>
2022-04-20 22:55:14 +0200

    Add: View Dreams page

--- src/pages/DreamsPage.tsx
+++ src/pages/DreamsPage.tsx
@@ -1,9 +1,44 @@
 import React from "react";
+import Table from "react-bootstrap/Table";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { Dream } from "../models/Dream";
+import { RootState } from "../store";
 
 export default function DreamsPage() {
+  const dreams = useSelector<RootState, Dream[]>(state => state.dreamReducer.dreams);
+
   return (
     <div>
-      Dreams page
+      <h1>All dreams</h1>
+      <Table striped>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Date</th>
+            <th>Type</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {dreams.map((dream, index) => {
+            return (
+              <tr key={index}>
+                <td>{dream.id}</td>
+                <td>{dream.title}</td>
+                <td>{dream.description}</td>
+                <td>{dream.date}</td>
+                <td>{dream.type}</td>
+                <td>
+                  <Link to={`/dreams/${dream.id}/edit`} className="button-primary">Edit</Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
     </div>
   );
 }
