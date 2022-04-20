71dc27a88f6c6772aa7807b6bc51bd3f17e64e51
mac4ever <borisknot@gmail.com>
2022-04-21 00:05:01 +0200

    Add: Form validations

--- src/components/DreamForm.tsx
+++ src/components/DreamForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
@@ -6,17 +6,41 @@ import Button from "react-bootstrap/Button";
 import { Dream, DreamType } from "../models/Dream";
 import { useDispatch } from "react-redux";
 import { add, update } from "../reducers/dreamSlice";
+import moment from "moment";
 
 export default function DreamForm({ dream }: { dream?: Dream }) {
   const [title, setTitle] = useState(dream ? dream.title : "");
   const [description, setDescription] = useState(dream ? dream.description : "");
-  const [date, setDate] = useState(dream ? dream.date : "");
+  const [date, setDate] = useState(dream ? dream.date : moment().format("DD-MM-YYYY"));
   const [type, setType] = useState(dream ? dream.type : DreamType.exciting);
+
+  const [isFormValid, setisFormValid] = useState(true);
+
+  const [titleValid, setTitleValid] = useState(true);
+  const [descriptionValid, setDescriptionValid] = useState(true);
+  const [dateValid, setDateValid] = useState(true);
+  const [typeValid, setTypeValid] = useState(true);
+
+  useEffect(() => {
+    setTitleValid(title !== "");
+    setDescriptionValid(description !== "");
+    setDateValid(moment(date, "DD-MM-YYYY", true).isValid());
+    setTypeValid(Object.values(DreamType).includes(type));
+  }, [
+    title, description, date, type,
+  ]);
+
+  useEffect(() => {
+    setisFormValid(titleValid && descriptionValid && dateValid && typeValid);
+  }, [titleValid, descriptionValid, dateValid, typeValid]);
 
   const dispatch = useDispatch();
 
   const submit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    if (!isFormValid) return;
+
     if (dream?.id) {
       dispatch(update({ id: dream.id!, title, description, date, type }));
     } else {
@@ -27,7 +51,7 @@ export default function DreamForm({ dream }: { dream?: Dream }) {
   return (
     <Row>
       <Col>
-        <Form onSubmit={submit}>
+        <Form onSubmit={submit} validated={isFormValid}>
           <Form.Group>
             <Form.Label>Title</Form.Label>
             <Form.Control
@@ -36,7 +60,7 @@ export default function DreamForm({ dream }: { dream?: Dream }) {
               value={title}
               onChange={e => setTitle(e.target.value)}
               required
-              isValid={false}
+              isInvalid={!titleValid}
             />
           </Form.Group>
 
@@ -48,7 +72,7 @@ export default function DreamForm({ dream }: { dream?: Dream }) {
               value={description}
               onChange={e => setDescription(e.target.value)}
               required
-              isValid={false}
+              isInvalid={!descriptionValid}
             />
           </Form.Group>
 
@@ -60,20 +84,20 @@ export default function DreamForm({ dream }: { dream?: Dream }) {
               value={date}
               onChange={e => setDate(e.target.value)}
               required
-              isValid={false}
+              isInvalid={!dateValid}
             />
           </Form.Group>
 
-          <Form.Group>
+          <Form.Group className="mb-4">
             <Form.Label>Dream type</Form.Label>
             <Form.Select
               aria-label="Choose dream type"
               name="type"
               onChange={e => setType(Number(e.target?.value))}
               required
-              isValid={false}
+              defaultValue={type}
+              isInvalid={!typeValid}
             >
-              <option>Choose dream type</option>
               <option value={DreamType.happy}>Happy</option>
               <option value={DreamType.sad}>Sad</option>
               <option value={DreamType.exciting}>Exciting</option>
